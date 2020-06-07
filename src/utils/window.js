9b278378b0e54d2ca9944e24ebfcf7fa2615f3cc
Mickaël Cornière <mcorniere.abo@gmail.com>
2020-06-07 21:56:46 +0200

    Force User Agent : Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0

--- src/utils/window.js
+++ src/utils/window.js
@@ -15,7 +15,7 @@ class Window extends BrowserWindow {
         };
         super(params);
         this.setMenuBarVisibility(false);
-        this.loadURL(url || "https://deezer.com");
+        this.loadURL(url || "https://deezer.com",{userAgent:'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0'});
         this._app = app;
         this.createEvents();
     }
